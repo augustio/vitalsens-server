a7afe7ebb735b349ae3a8584b4327295899fdaa3
augustio <augustine.onubeze@metropolia.fi>
2017-03-13 11:15:56 +0200

    Make dbURL in appConfig.js a local variable

--- config/appConfig.js
+++ config/appConfig.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-
+var dbURL;
 if(process.env.NODE_ENV == 'development')
     dbURL = "mongodb://83.136.249.208:27017/vitalsens";
 else
@@ -13,16 +13,16 @@ module.exports = {
             }
         });
     },
-    
+
     TOKEN_SECRET: "v1t4753n553cr3tk3y", //set this to a more elaborate value
-   
+
     REALM_NAME: "ospp-engine",
-    
+
     WAMP_ROUTER_URL: "ws://185.38.3.239:8000/ws",
-    
+
     PIPELINE_KEYS: ["rpeaks","rrintervals","pvcevents","hrvfeatures"],
-    
+
     OPEN_URL: "com.ospp.session.open",
-    
+
     CLOSE_URL: "com.ospp.session.close"
 }
